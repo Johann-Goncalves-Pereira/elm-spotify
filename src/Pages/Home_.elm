10cf882188be88f57f1e25f260833231be8830d9
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-03-16 17:12:27 -0300

    feat: :lipstick: main playlist section on the content of home

--- src/Pages/Home_.elm
+++ src/Pages/Home_.elm
@@ -1,12 +1,14 @@
 module Pages.Home_ exposing (Model, Msg, page)
 
+import Components.Svg as Svg
 import Gen.Params.Home_ exposing (Params)
 import Gen.Route as Route
-import Html exposing (Html, div, p, text)
-import Html.Attributes exposing (attribute, class, id, src, style)
+import Html exposing (Html, button, div, h3, h4, h5, header, img, p, section, text)
+import Html.Attributes exposing (alt, attribute, class, id, src, style)
 import Page
 import Request
 import Shared
+import Tuple exposing (second)
 import UI exposing (defaultConfig)
 import View exposing (View)
 
@@ -60,6 +62,43 @@ view model =
             { defaultConfig
                 | route = Route.Home_
                 , mainTagContent =
-                    [ p [] [ text "Home" ] ]
+                    [ viewMainPlaylist model ]
             }
     }
+
+
+viewMainPlaylist : Model -> Html Msg
+viewMainPlaylist model =
+    section
+        [ class "main-playlist"
+        , attribute "aria-labelledby" "main-playlist-heading"
+        ]
+        [ header [ class "header" ]
+            [ div [ class "header__btm-ctnr" ]
+                [ button [ class "header__btm-ctnr__arrows" ] [ Svg.arrowLine ]
+                , button [ class "header__btm-ctnr__arrows" ] [ Svg.arrowLine ]
+                ]
+            , button
+                [ class "header__user-ctnr" ]
+                [ img [ class "header__user-ctnr__img", src "https://picsum.photos/300", alt "User Photo" ] []
+                , h3 [ class "header__user-ctnr__heading", id "main-playlist-heading" ] [ text "Johann Gonçalves Pereira" ]
+                , Svg.arrowFull
+                ]
+            ]
+        , h4 [ class "main-playlist__heading" ] [ text "Good afternoon" ]
+        , List.repeat 6 ( "https://picsum.photos/500", "Liked Songs" )
+            |> List.map
+                (\( photo, album ) ->
+                    div [ class "playlist__wrapper" ]
+                        [ img
+                            [ class "playlist__wrapper__img"
+                            , src photo
+                            , alt <| "Album Photo of " ++ album
+                            ]
+                            []
+                        , h5 [ class "playlist__wrapper__album-name" ] [ text album ]
+                        , button [ class "playlist__wrapper__player" ] [ Svg.play Svg.Playing ]
+                        ]
+                )
+            |> div [ class "playlist" ]
+        ]
