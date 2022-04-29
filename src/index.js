b10bc0d81ec8134cd06c6d027095312f42e96272
Johann Gonçalves Pereira <johann.gon.pereira@gmail.com>
2022-04-29 16:22:33 -0300

    feat:idk

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import { Elm } from "./Main.elm";
 const stylesL00 = import.meta.globEager("./Styles/*.scss");
 const stylesL01 = import.meta.globEager("./Styles/**/*.scss");
 
-const app = Elm.Main.init();
+const app = Elm.Main.init({
+  node: document.getElementById("root"),
+});
